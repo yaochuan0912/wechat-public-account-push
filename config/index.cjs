335d2430309c4b7f80aef9230c607dd1812e8d38
yaochuan0912 <90554079+yaochuan0912@users.noreply.github.com>
2022-12-09 20:45:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,30 +19,30 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '李欣悦',
+      name: '小宝儿',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ox2PX5iXhaqJ0gQKryoFBKat-YqA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'YGJhA3cW55d9ytpraMCyjiwr5P3mGsHCRJyb_zGZtTc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '4-8',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '宝贝', year: '2002', date: '02-26',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '李四', year: '2002', date: '04-8',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2022', date: '10-31',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-11-9' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
